7f4ba202ba465bf273b2ecb7f83899a111306997
Raja Jawahar <rajajawahar77@gmail.com>
2017-04-19 20:52:43 +0530

    ButterKnife added

--- app/src/main/java/com/silicontechnologies/drawonimageview/HomeActivity.java
+++ app/src/main/java/com/silicontechnologies/drawonimageview/HomeActivity.java
@@ -1,13 +1,47 @@
 package com.silicontechnologies.drawonimageview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.imageview)
+    ImageView imageView;
+    @BindView(R.id.save_image)
+    Button saveImage;
+    @BindView(R.id.action_camera)
+    Button actionCamera;
+    @BindView(R.id.action_gallery)
+    Button actionGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+    }
+
+
+    //    Button action
+    @OnClick({R.id.action_gallery,
+            R.id.action_camera,
+            R.id.save_image})
+    public void OnActionClick(View view) {
+        switch (view.getId()) {
+            case R.id.action_camera:
+                break;
+            case R.id.action_gallery:
+                break;
+            case R.id.save_image:
+                break;
+        }
     }
 }
