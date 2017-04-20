7a054170125f106365223451bbd03e1835eaa0ea
Raja Jawahar <rajajawahar77@gmail.com>
2017-04-20 08:58:00 +0530

    Camera Option added

--- app/src/main/java/com/silicontechnologies/drawonimageview/HomeActivity.java
+++ app/src/main/java/com/silicontechnologies/drawonimageview/HomeActivity.java
@@ -1,10 +1,16 @@
 package com.silicontechnologies.drawonimageview;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,14 +19,13 @@ import butterknife.OnClick;
 public class HomeActivity extends AppCompatActivity {
 
 
+    private int PICK_IMAGE_REQUEST = 1;
+    private static final int CAMERA_REQUEST = 1888;
+
+
     @BindView(R.id.imageview)
     ImageView imageView;
-    @BindView(R.id.save_image)
-    Button saveImage;
-    @BindView(R.id.action_camera)
-    Button actionCamera;
-    @BindView(R.id.action_gallery)
-    Button actionGallery;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +35,49 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
-    //    Button action
     @OnClick({R.id.action_gallery,
             R.id.action_camera,
             R.id.save_image})
     public void OnActionClick(View view) {
         switch (view.getId()) {
             case R.id.action_camera:
+                selectImageFromCamera();
                 break;
             case R.id.action_gallery:
+                selectImageFromGallery();
                 break;
             case R.id.save_image:
                 break;
+        }
+    }
+
+    public void selectImageFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    public void selectImageFromCamera() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
         }
     }
 }
